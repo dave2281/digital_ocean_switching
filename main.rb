b097f771f205c7b0a956ded68823dbc98a7b8b4b
David <82700142+dave2281@users.noreply.github.com>
2024-06-08 16:48:15 +0000

    Added main file

---
+++ main.rb
@@ -0,0 +1,16 @@
+require 'droplet_kit'
+
+client = DropletKit::Client.new(access_token: 'API_token') # Api token (I did custom api, droplets[read, update])
+droplet_id = 'DROPLET_ID' # Your droplet id
+
+action_type = ARGV[0]
+
+if action_type == 'on'
+  client.droplet_actions.power_on(droplet_id: droplet_id)
+  puts "Droplet #{droplet_id} is powering on."
+elsif action_type == 'off'
+  client.droplet_actions.power_off(droplet_id: droplet_id)
+  puts "Droplet #{droplet_id} is powering off."
+else
+  puts "Invalid action. Use 'start' or 'stop'."
+end
