ddc532ee3a10d57e38bce845e111843721c2b04a
David <82700142+dave2281@users.noreply.github.com>
2024-06-11 08:02:47 +0000

    changed elsif to case-when. added snapshot

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # digital_ocean_switching
-Ruby programm which turning on\off your digitalOcean droplet
+Ruby programm which turning on\off your digitalOcean droplet.
+
+
+on - power on your droplet             
+`ruby filename.rb on`
+off - power off your droplet           
+`ruby filename.rb off`
+snap - make snapshot of your droplet.   
+`ruby filename.rb snap snapshotName`
--- main.rb
+++ main.rb
@@ -1,19 +1,24 @@
 require 'droplet_kit'
 
-client = DropletKit::Client.new(access_token: 'API_token') # Api token (I did custom api, droplets[read, update])
-droplet_id = 'DROPLET_ID' # Your droplet id
+client = DropletKit::Client.new(access_token: 'API_token')
+droplet_id = 'DROPLET_ID'
 
 action_type = ARGV[0]
 
-if action_type == 'on'
+case action_type
+when 'on'
   client.droplet_actions.power_on(droplet_id: droplet_id)
   puts "Droplet #{droplet_id} is powering on."
-elsif action_type == 'off'
+when 'off'
   client.droplet_actions.power_off(droplet_id: droplet_id)
   puts "Droplet #{droplet_id} is powering off."
-elsif action_type == 'reboot'  
-  client.droplet_actions.reboot(droplet_id: droplet_id)
-  puts "Droplet #{droplet_id} is rebooting."
+when 'snap'
+  client.droplet_actions.snapshot(droplet_id: droplet_id, name: "#{ARGV[1]}")
+  puts "Droplet #{droplet_id} is making snapshot."
+when 'help'
+  puts 'on - power on your droplet             --> `ruby filename.rb on`'
+  puts 'off - power off your droplet           --> `ruby filename.rb off`'
+  puts 'snap - make snapshot of your droplet.  --> `ruby filename.rb snap snapshotName`'
 else
-  puts "Invalid action. Use 'start' or 'stop'."
+  puts "Invalid action. Use 'off' or 'on'."
 end
